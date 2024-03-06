8ad786aa0b1cafcc6b3d8d2f0a953887adf92982
Filip Ryan <filipryanbusinessmail@gmail.com>
2024-03-06 21:16:39 +0100

    testing git

---
+++ Src/App/main.js
@@ -0,0 +1,89 @@
+window.addEventListener("DOMContentLoaded", () => {
+  //console.log("domcontent loaded");
+  let intro = document.querySelector(".intro");
+  let introLabel = document.querySelectorAll(".introLabel");
+  let header = document.querySelector(".header");
+
+  setTimeout(() => {
+    introLabel.forEach((span, i) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (i + 1) * 225);
+    });
+
+    setTimeout(() => {
+      introLabel.forEach((span, i) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (i + 1) * 225);
+      });
+    }, 2750);
+
+    setTimeout(() => {
+      gsap.to(intro, {
+        duration: 0.3,
+        top: "-150dvh",
+        ease: "power4.inOut",
+      });
+      //intro.style.top = "-150dvh";
+    }, 3800);
+
+    setTimeout(() => {
+      gsap.to(header, {
+        y: 0,
+      });
+    }, 4700);
+  }, 0);
+});
+
+let navbarToggleButton = document.querySelector("#navbarToggleButton");
+let navbar = document.querySelector(".navbar .content");
+let menuOpen = false;
+let navbarTL = gsap.timeline();
+
+navbarToggleButton.addEventListener("click", () => {
+  if (!menuOpen) {
+    navbarTL.to(":root", {
+      duration: 1,
+      "--nl": "100%",
+      "--nr": "86%",
+      ease: "power4.inOut",
+      /*
+      onUpdate: function (progress) {
+        console.log("Progress:", progress); // Debug
+        const INTERPOLATED_VALUE = interpolateValues(86, 70, progress);
+        console.log("Interpolated value:", INTERPOLATED_VALUE); // Debug
+        document.documentElement.style.setProperty(
+          "--nr",
+          INTERPOLATED_VALUE + "%"
+        );
+      },
+      */
+    });
+    menuOpen = true;
+  } else {
+    navbarTL.to(":root", {
+      duration: 1,
+      "--nl": "0%",
+      "--nr": "0%",
+      ease: "power4.inOut",
+      /*
+      onUpdate: function (progress) {
+        console.log("Progress:", progress); // Debug
+        const INTERPOLATED_VALUE = interpolateValues(70, 86, progress);
+        console.log("Interpolated value:", INTERPOLATED_VALUE); // Debug
+        document.documentElement.style.setProperty(
+          "--nr",
+          INTERPOLATED_VALUE + "%"
+        );
+      },
+      */
+    });
+    menuOpen = false;
+  }
+});
+
+function interpolateValues(startValue, endValue, progress) {
+  return startValue + (endValue - startValue) * progress;
+}
